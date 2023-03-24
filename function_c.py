811c39e839a080dd791492a4db3b43c697f96ae1
halimAremu <halimat3000@gmail.com>
2023-03-24 16:28:09 -0500

    edited function c

--- function_c.py
+++ function_c.py
@@ -3,7 +3,8 @@ def merge_lists(list_a, list_b):
         a combination of list_a and list_b
         without any duplicate elements.
     """
-    pass
+    result = list_a.extend(list_b)
+    return result
 
 
 if __name__ == "__main__":
